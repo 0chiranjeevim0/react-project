fed3fce6f2de4459ba2bdebd0d8a178dd5ec227d
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-01-26 15:30:29 +0530

    status:form regex

--- src/schema/loginSchema.js
+++ src/schema/loginSchema.js
@@ -3,11 +3,10 @@ import * as Yup from 'yup'
 const loginSchema = Yup.object().shape({
     
     email:Yup.string().email().required('Required'),
-    password:Yup.string().min(8,"Password is too short").required('Required')
+    password:Yup.string().min(4,"Password is too short").required('Required')
     .matches(/[0-9]/,'Password Requires a number')
     .matches(/[a-z]/,'Password requires lowercase')
-    .matches(/[A_Z]/,'Password requires an uppercase')
-    .matches(/[^\w]/,'Password requires a symbol'),
+    .matches(/[A-Z]/,'Password requires an uppercase'),
     name:Yup.string().max(20,"Name is too long").required('Required'),
     number:Yup.string().max(10,"Enter a valid phone number").required('Required'),
 
